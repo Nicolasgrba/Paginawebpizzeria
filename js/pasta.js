bd816f4db0c226bc3c5761fa343421771be28091
cesar18012 <cesardelcastillo18012@gmail.com>
2023-11-06 09:59:14 +0100

    pullcesar

---
+++ js/pasta.js
@@ -0,0 +1,15 @@
+fetch('json/pasta.json')
+  .then(response => response.json())
+  .then(data => {
+    var pasta = data.carta_de_pasta;
+    var pastaList = document.querySelector('.pasta');
+
+    pasta.forEach(pasta => {
+      var pastaItem = document.createElement('li');
+      pastaItem.innerHTML = `<h3><u>${pasta.nombre}</u></h3>
+                              <p> ${pasta.ingredientes.join(', ')}</p>
+                              <p> ${pasta.precio}€</p>`;
+      pastaList.appendChild(pastaItem);
+    });
+  });
+
